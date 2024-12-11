d27a9c4b2b277b2ac47df3d8fc4664f00be6dab3
ashmitashahi9 <ashmitashahi67@gmail.com>
2024-12-11 12:59:34 +0545

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:calculator/view/calculator_screen_view.dart';
+import 'package:flutter/material.dart';
+
+
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/', // Initial Route
+      routes: {
+        '/': (context) => const CalculatorScreen(),
+      },
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:calculator/app.dart';
+import 'package:flutter/material.dart';
+
+
+//Hot Reload
+void main() {
+  //Run App
+  runApp(const App());
+}
